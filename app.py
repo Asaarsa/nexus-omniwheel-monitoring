e1bc07d9d278ddd34f9e4103c4e27d1ac651a7a4
Asa arsa <158986386+Asaarsa@users.noreply.github.com>
2026-05-11 22:43:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if 'last_command' not in st.session_state:
 
 # --- SIDEBAR: KONTROL MODE & KONFIRMASI ---
 st.sidebar.title("🤖 Robot Control")
-st.sidebar.write("Apriyana Putra - Polibatam")
+st.sidebar.write("TEAM PBL NEXUS - Polibatam")
 
 selected_mode = st.sidebar.radio(
     "Pilih Mode Operasi:",
